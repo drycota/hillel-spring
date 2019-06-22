82f36fc51195b4d1271d12ae0d7f944a2ff616d1
drycota <drycota@gmail.com>
2019-06-22 11:47:14 +0300

    init

---
+++ src/main/java/hillel/spring/Apple.java
@@ -0,0 +1,21 @@
+package hillel.spring;
+
+public class Apple {
+    private final Integer weight;
+    private final String color;
+
+    public Apple(Integer weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
+
