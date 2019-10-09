29c31fe232ea47dfb9521be59faa3dbdd71eee47
drycota <drycota@gmail.com>
2019-10-09 16:20:41 +0300

    testcontainers

--- src/test/java/hillel/spring/ApplicationTests.java
+++ src/test/java/hillel/spring/ApplicationTests.java
@@ -2,11 +2,10 @@ package hillel.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@TestRunner
 public class ApplicationTests {
 
 	@Test
--- src/test/java/hillel/spring/petclinic/pet/PetControllerTest.java
+++ src/test/java/hillel/spring/petclinic/pet/PetControllerTest.java
@@ -1,11 +1,10 @@
 package hillel.spring.petclinic.pet;
 
+import hillel.spring.TestRunner;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,8 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@TestRunner
 public class PetControllerTest {
 
     @Autowired
